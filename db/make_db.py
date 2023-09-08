51798556ca3c97155b0bf5f3314a18e00563724a
petterbejo <petterbj@outlook.dk>
2023-09-08 09:47:41 +0200

    Correct location format to comply with DD

--- db/make_db.py
+++ db/make_db.py
@@ -10,15 +10,16 @@ conn = sqlite3.connect(db_path)
 create_tables = """ CREATE TABLE IF NOT EXISTS markers (
                                     id integer PRIMARY KEY,
                                     name text NOT NULL,
-                                    location text
+                                    DD_latitude REAL,
+                                    DD_longitude REAL
                                 ); """
 
 c = conn.cursor()
 c.execute(create_tables)
 
-records = [(1, 'test1', '55.73931038034454, 12.24586002073197'),
-           (2, 'test2', '55.703214, 12.485766')]
-c.executemany('INSERT INTO markers VALUES(?, ?, ?);', records)
+records = [(1, 'test1', 55.73931038034454, 12.24586002073197),
+           (2, 'test2', 55.703214, 12.485766)]
+c.executemany('INSERT INTO markers VALUES(?, ?, ?, ?);', records)
 
 conn.commit()
 conn.close()
