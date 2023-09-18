de70546905ab29153f9cc5073d9226a592f5b4b9
petterbejo <petterbj@outlook.dk>
2023-09-18 10:14:04 +0200

    Save new single and correctly formatted marker to db

--- app/data_handler.py
+++ app/data_handler.py
@@ -13,6 +13,18 @@ class DataHandler():
         """Open a connection to the database."""
         return sqlite3.connect(self.db_path)
 
+    def _format_location(self, location):
+        """Checks that the location is formatted in decimal degrees.
+
+        For the  precision (see
+        https://en.wikipedia.org/wiki/Decimal_degrees#Precision), at
+        least one decimal place (e.g. 55.7, 12.4) is required."""
+        location_split = location.split(',')
+        latitude = location_split[0].replace(' ', '')
+        longitude = location_split[1].replace(' ', '')
+        return latitude, longitude
+
+
     def get_all_categories(self):
         conn = self._get_db_connection()
         cur = conn.cursor()
@@ -21,7 +33,6 @@ class DataHandler():
         cur.close()
         conn.cursor()
         return rows
-
 
     def get_all_markers(self):
         conn = self._get_db_connection()
@@ -42,21 +53,27 @@ class DataHandler():
         return row
 
     def insert_new_single_marker(self, insert_dict):
-        print(insert_dict)
-        # check that data is correct
-    '''
-    data_insert_str = (f"INSERT INTO markers (name, description, "
-                       f"category, link, access_distance, rating, visited, "
-                       f"country_code, region, DD_latitude, DD_longitude) "
-                       f"VALUES ({request.form['name']}, "
-                       f"{request.form['description']}," f" "
-                       f"{request.form['category']},"
-                       f" {request.form['link']}, "
-                       f"{request.form['access_distance']},"
-                       f" {request.form['rating']}, {request.form['visited']},"
-                       f" {request.form['country_code']},"
-                       f" {request.form['region']},"
-                       f" {request.form['']}, )")
-    f" {request.form['name']}, )")
-    print(data_insert_str)
-'''
+        try:
+            latitude, longitude = self._format_location(
+                insert_dict['location'])
+            data_insert_str = (f"""INSERT INTO markers (name, description, """
+                       f"""category, link, access_distance, rating, visited, """
+                       f"""country_code, region, DD_latitude, DD_longitude) """
+                       f"""VALUES ('{insert_dict["name"]}', """
+                       f"""'{insert_dict["description"]}', """ 
+                       f"""'{insert_dict["category"]}', """
+                       f"""'{insert_dict["link"]}', """
+                       f"""'{insert_dict["access_distance"]}', """
+                       f"""'{insert_dict["rating"]}', '{insert_dict["visited"]}', """
+                       f"""'{insert_dict["country_code"]}', """
+                       f"""'{insert_dict["region"]}', """
+                       f"""'{latitude}', """
+                       f"""'{longitude}' )""")
+            print(data_insert_str)
+            conn = self._get_db_connection()
+            cur = conn.cursor()
+            cur.execute(data_insert_str)
+            conn.commit()
+            conn.close()
+        except Exception as e:
+            print(e)
